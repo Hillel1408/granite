0221bd86eb5dd500fde6cd9f9a7e10f62804cb05
Hillel1408 <etemax@bk.ru>
2023-05-16 15:06:20 +0300

    granite

---
+++ #src/js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.slider', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.slider__swiper-button-next',
+        prevEl: '.slider__swiper-button-prev',
+    },
+});
