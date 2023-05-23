9f1ef845bfd507c8e01d845ed84a4e1eaba41535
Hillel1408 <etemax@bk.ru>
2023-05-23 17:12:39 +0300

    granite

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,57 +1,3 @@
-const swiper = new Swiper('.slider', {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.slider__swiper-button-next',
-        prevEl: '.slider__swiper-button-prev',
-    },
-    scrollbar: {
-        el: '.slider__swiper-scrollbar',
-        draggable: true,
-        snapOnRelease: true,
-        dragSize: 'auto',
-    },
-});
-const bestOfferSwiper = new Swiper('.best-offer-slider', {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.best-offer__swiper-button-next',
-        prevEl: '.best-offer__swiper-button-prev',
-    },
-    enabled: false,
-    breakpoints: {
-        576: {
-            enabled: true,
-        },
-    },
-});
-const productSwiper = new Swiper('.product-slider', {
-    direction: 'horizontal',
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    navigation: {
-        nextEl: '.product__swiper-button-next',
-        prevEl: '.product__swiper-button-prev',
-    },
-});
-const productSwiper2 = new Swiper('.product-slider-2', {
-    direction: 'horizontal',
-    slidesPerView: 'auto',
-    navigation: {
-        nextEl: '.product__swiper-button-next-2',
-        prevEl: '.product__swiper-button-prev-2',
-    },
-});
-const productSwiper3 = new Swiper('.product-slider-3', {
-    direction: 'horizontal',
-    slidesPerView: 'auto',
-    navigation: {
-        nextEl: '.product__swiper-button-next-3',
-        prevEl: '.product__swiper-button-prev-3',
-    },
-});
-
 const headerNavBtn = document.querySelector('.header-nav__btn');
 const sidebar = document.querySelector('.sidebar-wrapper');
 const popup = document.querySelector('.popup');
@@ -78,24 +24,6 @@ headerNavBtn.addEventListener('click', function () {
             'px';
 });
 
-const tabsItems = document.querySelectorAll('.reviews-tabs-item');
-const tabsBlock = document.querySelectorAll('.reviews-tabs-block');
-
-for (let elem of tabsItems) {
-    elem.addEventListener('click', function () {
-        for (let elem of tabsItems) {
-            elem.classList.remove('active');
-        }
-        this.classList.add('active');
-        for (let elem of tabsBlock) {
-            elem.classList.remove('active');
-            if (this.dataset.id == elem.dataset.id) {
-                elem.classList.add('active');
-            }
-        }
-    });
-}
-
 const headerNav = document.querySelector('.header-nav');
 const menuBtn = document.querySelector('.header__menu-btn');
 const closeBtn = document.querySelector('.header-nav__close-btn');
@@ -115,26 +43,5 @@ const mobileSidebar = document.querySelector('.sidebar');
 
 mobileMenu.addEventListener('click', function () {
     mobileSidebar.classList.toggle('open');
-    document.body.classList.toggle('lock');
-});
-
-const cartBtn = document.querySelector('.cart__form-btn');
-const popup2 = document.querySelector('.popup-2');
-const popup2Close = document.querySelector('.popup-2__close');
-const popup2Btn = document.querySelector('.popup-2__btn');
-
-cartBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    popup2.classList.toggle('open');
-    document.body.classList.toggle('lock');
-});
-
-popup2Close.addEventListener('click', function () {
-    popup2.classList.toggle('open');
-    document.body.classList.toggle('lock');
-});
-
-popup2Btn.addEventListener('click', function () {
-    popup2.classList.toggle('open');
     document.body.classList.toggle('lock');
 });
